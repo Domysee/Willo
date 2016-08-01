d56f8cca233a5cbd1ce1b1a87f32ca2c4eb97f8b
Dominik Weber <accounts@dominik-weber.name>
2016-08-01 23:17:40 +0200

    Add Authorization Option Classes

---
+++ Tapi/Authorization/AuthorizationExpiration.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tapi.Authorization
+{
+    public class AuthorizationExpiration
+    {
+        public const string OneHour = "1hour";
+        public const string OneDay = "1day";
+        public const string ThirtyDays = "30days";
+        public const string Never = "never";
+    }
+}
---
+++ Tapi/Authorization/AuthorizationScope.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tapi.Authorization
+{
+    /// <summary>
+    /// read and write are self-explanatory
+    /// account includes writing of member info and marking notifications as read
+    /// as described here: https://developers.trello.com/authorize
+    /// </summary>
+    public class AuthorizationScope
+    {
+        public const string ReadOnly = "read";
+        public const string ReadWrite = "read,write";
+        public const string ReadOnlyAccount = "read,account";
+        public const string ReadWriteAccount = "read,write,account";
+    }
+}
