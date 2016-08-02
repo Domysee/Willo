1e39cc6a94fe6a6720037d9c53b92e9def9cb331
Dominik Weber <accounts@dominik-weber.name>
2016-08-02 19:54:15 +0200

    Add TrelloWebClient Class and a Value Object for the Authorization Token

---
+++ Tapi/WebConnection/AuthorizationToken.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tapi.WebConnection
+{
+    public struct AuthorizationToken
+    {
+        private string token;
+
+        public AuthorizationToken(string token)
+        {
+            this.token = token;
+        }
+
+        public static implicit operator AuthorizationToken(string token)
+        {
+            return new AuthorizationToken(token);
+        }
+
+        public static explicit operator string(AuthorizationToken token)
+        {
+            return token.token;
+        }
+    }
+}
---
+++ Tapi/WebConnection/TrelloWebClient.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tapi.WebConnection
+{
+    public class TrelloWebClient
+    {
+        private AuthorizationToken authorizationToken;
+
+        public void Authorize(AuthorizationToken authorizationToken)
+        {
+            this.authorizationToken = authorizationToken;
+        }
+    }
+}
