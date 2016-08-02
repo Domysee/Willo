2e0c3ca446ae509a1774ea5ffaf4827f25dea81b
Dominik Weber <accounts@dominik-weber.name>
2016-08-02 21:34:34 +0200

    Add a generic Way to receive Data via Get Requests

--- Tapi/WebConnection/TrelloWebClient.cs
+++ Tapi/WebConnection/TrelloWebClient.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.Web.Http;
 
 namespace Tapi.WebConnection
 {
@@ -13,6 +15,18 @@ namespace Tapi.WebConnection
         public void Authorize(AuthorizationToken authorizationToken)
         {
             this.authorizationToken = authorizationToken;
+        }
+
+        public async Task<T> Get<T>(string url)
+        {
+            var uri = new Uri(url, UriKind.Absolute);
+            using (var client = new HttpClient())
+            {
+                var response = await client.GetAsync(uri);
+                var responseJson = await response.Content.ReadAsStringAsync();
+                var result = await Task.Run(() => JsonConvert.DeserializeObject<T>(responseJson));
+                return result;
+            }
         }
     }
 }
