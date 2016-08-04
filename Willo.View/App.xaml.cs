e5284bb10dcc9a5936958a01cb2ddb3f2a97d51f
Dominik Weber <accounts@dominik-weber.name>
2016-08-04 22:17:56 +0200

    Rename Willo to Willo.View and add Willo.Logic
    Willo.Logic will contain all logic, WIllo.View is only for display. This clearly separates logic and view,
    and will hopefully lead to a less messy codebase.

--- Willo.View/App.xaml.cs
+++ Willo.View/App.xaml.cs
@@ -15,7 +15,7 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-namespace Willo
+namespace Willo.View
 {
     /// <summary>
     /// Provides application-specific behavior to supplement the default Application class.
