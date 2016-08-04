bd8cd3732f5387e222d7dd96d0c0e1705f7eac2d
Dominik Weber <accounts@dominik-weber.name>
2016-08-04 20:34:31 +0200

    Add Trello Class, the main Entry Point for Users of this Trello Api Wrapper

---
+++ Tapi/Trello.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tapi.Authorization;
+using Tapi.TrelloEntities.Board;
+using Tapi.WebConnection;
+
+namespace Tapi
+{
+    /// <summary>
+    /// Trello is the entry point for users of the api
+    /// </summary>
+    public class Trello
+    {
+        private TrelloWebClient webClient;
+
+        public Boards Boards { get; }
+
+        public Trello()
+        {
+            webClient = new TrelloWebClient();
+            Boards = new Boards(webClient);
+        }
+
+        public void Authorize(ApplicationKey applicationKey, AuthorizationToken authorizationToken)
+        {
+            webClient.Authorize(applicationKey, authorizationToken);
+        }
+    }
+}
