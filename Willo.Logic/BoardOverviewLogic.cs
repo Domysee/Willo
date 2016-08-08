b6114587de20e4561ba96b7d479d1ca6ea431f21
Dominik Weber <accounts@dominik-weber.name>
2016-08-08 20:11:10 +0200

    Add Logic and Viewmodel Class for BoardOverview

---
+++ Willo.Logic/BoardOverviewLogic.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tapi;
+using Tapi.TrelloEntities;
+using Tapi.TrelloEntities.Board;
+
+namespace Willo.Logic
+{
+    public class BoardOverviewLogic
+    {
+        private Trello api;
+
+        public BoardOverviewLogic(Trello api)
+        {
+            this.api = api;
+        }
+
+        public async Task<IEnumerable<Board>> GetBoards()
+        {
+            return await api.Boards.GetAll(MemberId.AuthorizedUser);
+        }
+    }
+}
---
+++ Willo.View/Components/BoardOverview/BoardOverviewViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Willo.View.Components.BoardOverview
+{
+    public class BoardOverviewViewModel
+    {
+        public void Initialize()
+        {
+
+        }
+    }
+}
