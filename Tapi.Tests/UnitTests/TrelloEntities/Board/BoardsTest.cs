71ae999bfb1f579fcc810680a5a9d8fda76be80a
Dominik Weber <accounts@dominik-weber.name>
2016-08-10 22:04:03 +0200

    Add Tests for Tapi Boards

---
+++ Tapi.Tests/UnitTests/TrelloEntities/Board/BoardsTest.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tapi.TrelloEntities.Board;
+using Xunit;
+
+namespace Tapi.Tests.UnitTests.TrelloEntities.Board
+{
+    public class BoardsTest
+    {
+        [Fact]
+        public void GetAllShouldThrowExceptionIfMemberIdIsNull()
+        {
+            var boards = new Boards(new WebConnection.TrelloWebClient());
+            Action getAll = () => { boards.GetAll(null).Wait(); };
+
+            getAll.ShouldThrow<ArgumentNullException>();
+        }
+    }
+}
--- Tapi/TrelloEntities/Board/Boards.cs
+++ Tapi/TrelloEntities/Board/Boards.cs
@@ -17,8 +17,16 @@ namespace Tapi.TrelloEntities.Board
             this.webClient = webClient;
         }
 
+        /// <summary>
+        /// gets all boards of the given user
+        /// </summary>
+        /// <param name="memberId">the user to get the boards from</param>
+        /// <param name="properties">the properties that should be returned by the api</param>
+        /// <returns></returns>
         public async Task<IEnumerable<Board>> GetAll(MemberId memberId, BoardProperties properties = null)
         {
+            if (memberId == null) throw new ArgumentNullException("The given memberId is null. To use the authorized user, use MemberId.AuthorizedUser");
+
             var url = $"{ConnectionData.BaseUrl}/members/{memberId}/boards";
             if (properties != null)
                 url += "?fields=" + properties.ToString();
