8c8e91878d549ec3944f03516286a73b0bfa0092
Dominik Weber <accounts@dominik-weber.name>
2016-08-14 01:59:33 +0200

    Change TrelloWebClient to use IHttpFilter instead of simply appending the Authorization Parameters

--- Tapi/WebConnection/TrelloWebClient.cs
+++ Tapi/WebConnection/TrelloWebClient.cs
@@ -9,11 +9,42 @@ using Tapi.Authorization;
 using Windows.Web.Http;
 using Windows.Web.Http.Filters;
 using Windows.Web.Http.Headers;
+using Windows.Foundation;
 
 namespace Tapi.WebConnection
 {
     public class TrelloWebClient : ITrelloWebClient
     {
+        private class AddCredentialsFilter : IHttpFilter
+        {
+            private ApplicationKey applicationKey;
+            private AuthorizationToken authorizationToken;
+            private IHttpFilter next;
+
+            public AddCredentialsFilter(ApplicationKey applicationKey, AuthorizationToken authorizationToken, IHttpFilter next)
+            {
+                this.applicationKey = applicationKey;
+                this.authorizationToken = authorizationToken;
+                this.next = next;
+            }
+
+            public IAsyncOperationWithProgress<HttpResponseMessage, HttpProgress> SendRequestAsync(HttpRequestMessage request)
+            {
+                var builder = new UriBuilder(request.RequestUri);
+                var authorization = $"key={applicationKey}&token={authorizationToken}";
+                if (String.IsNullOrWhiteSpace(builder.Query))
+                    builder.Query = authorization;
+                else
+                    builder.Query = builder.Query.Remove(0, 1) + $"&{authorization}";   //remove the questionmark at the beginning
+                request.RequestUri = builder.Uri;
+                return next.SendRequestAsync(request);
+            }
+
+            public void Dispose()
+            {
+            }
+        }
+
         public bool IsAuthorized { get; private set; }
         private ApplicationKey applicationKey;
         private AuthorizationToken authorizationToken;
@@ -33,29 +64,20 @@ namespace Tapi.WebConnection
             {
                 AllowUI = false
             };
-            var client = new HttpClient(httpBaseFilter);
+            var addCredentialsFilter = new AddCredentialsFilter(applicationKey, authorizationToken, httpBaseFilter);
+            var client = new HttpClient(addCredentialsFilter);
             return client;
         }
 
-        private void appendAuthorizationParameter(UriBuilder uri)
-        {
-            var authorization = $"key={applicationKey}&token={authorizationToken}";
-            if (String.IsNullOrWhiteSpace(uri.Query))
-                uri.Query = authorization;
-            else
-                uri.Query += $"&{authorization}";
-        }
-
-        private void throwIfNotAuthorized()
+        private void checkAuthorization()
         {
             if (!IsAuthorized) throw new UnauthorizedException();
         }
 
         public async Task<JToken> Get(string url)
         {
-            throwIfNotAuthorized(); 
+            checkAuthorization();
             var uri = new UriBuilder(url);
-            appendAuthorizationParameter(uri);
             using (var client = createClient())
             {
                 var response = await client.GetAsync(uri.Uri);
