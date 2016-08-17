b41ea6c7705a91ed59dbba217dc5bcaf40b303ef
Dominik Weber <accounts@dominik-weber.name>
2016-08-17 20:55:54 +0200

    Add Unit Tests for AuthorizeCommandHandler

---
+++ Willo.Logic.Tests/Login/AuthorizeCommandHandlerTests.cs
@@ -0,0 +1,32 @@
+﻿using HyperMock.Universal;
+using HyperMock.Universal.Verification;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tapi;
+using Tapi.Authorization;
+using Tapi.WebConnection;
+using Willo.Logic.Login;
+
+namespace Willo.Logic.Tests.Login
+{
+    [TestClass]
+    public class AuthorizeCommandHandlerTests
+    {
+        [TestMethod]
+        public void ShouldAuthorizeApi()
+        {
+            var token = "authorizationtokenauthorizationtokenauthorizationtoken1234567890";
+            var api = Mock.Create<ITrello>();
+            api.Setup(a => a.Authorize(Param.IsAny<ApplicationKey>(), Param.IsAny<AuthorizationToken>()));
+            var handler = new AuthorizeCommandHandler(api);
+
+            handler.Handle(new AuthorizeCommand(token)).Wait();
+
+            api.Verify(a => a.Authorize(Param.IsAny<ApplicationKey>(), Param.IsAny<AuthorizationToken>()), Occurred.Once());
+        }
+    }
+}
