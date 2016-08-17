5a96538ad5ca493f8873793ca0fe1ecc2caf972d
Dominik Weber <accounts@dominik-weber.name>
2016-08-17 20:50:33 +0200

    Add Unit Tests for IsAuthorizationTokenQueryHandler

---
+++ Willo.Logic.Tests/Login/IsAuthorizationTokenQueryHandlerTests.cs
@@ -0,0 +1,63 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Willo.Logic.Login;
+
+namespace Willo.Logic.Tests.Login
+{
+    [TestClass]
+    public class IsAuthorizationTokenQueryHandlerTests
+    {
+        [TestMethod]
+        public void ShouldReturnFalseIfTokenContainsInvalidCharacters()
+        {
+            var invalidToken = "#$^%$%&^$%";
+            var query = new IsAuthorizationTokenQuery(invalidToken);
+            var handler = new IsAuthorizationTokenQueryHandler();
+
+            var result = handler.Handle(query).Result;
+
+            result.Should().BeFalse();
+        }
+
+        [TestMethod]
+        public void ShouldReturnFalseIfTokenIsTooLong()
+        {
+            var invalidToken = "abcdefghijklmnopqrstuvwxyz1234567890abcdefghijklmnopqrstuvwxyz1234567890abcdefghijklmnopqrstuvwxyz1234567890abcdefghijklmnopqrstuvwxyz1234567890abcdefghijklmnopqrstuvwxyz1234567890abcdefghijklmnopqrstuvwxyz1234567890";
+            var query = new IsAuthorizationTokenQuery(invalidToken);
+            var handler = new IsAuthorizationTokenQueryHandler();
+
+            var result = handler.Handle(query).Result;
+
+            result.Should().BeFalse();
+        }
+
+        [TestMethod]
+        public void ShouldReturnFalseIfTokenIsTooShort()
+        {
+            var invalidToken = "abcdef";
+            var query = new IsAuthorizationTokenQuery(invalidToken);
+            var handler = new IsAuthorizationTokenQueryHandler();
+
+            var result = handler.Handle(query).Result;
+
+            result.Should().BeFalse();
+        }
+
+        [TestMethod]
+        public void ShouldReturnTrueIfTokenIsValid()
+        {
+            var validToken = "authorizationtokenauthorizationtokenauthorizationtoken1234567890";
+            var query = new IsAuthorizationTokenQuery(validToken);
+            var handler = new IsAuthorizationTokenQueryHandler();
+
+            var result = handler.Handle(query).Result;
+
+            result.Should().BeTrue();
+        }
+    }
+}
