712e2f1f15b7e6a0f3c13c85510dc9a5073d982c
Dominik Weber <accounts@dominik-weber.name>
2016-08-28 15:24:14 +0200

    Add LambdaCommandHandler
    This class allows viewmodels to define local command handler that have access to
    the viewmodel's properties.

---
+++ Willo.Logic/LambdaCommandHandler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Willo.Logic
+{
+    public class LambdaCommandHandler<TCommand> : CommandHandlerBase<TCommand>
+        where TCommand : ICommand
+    {
+        private Func<TCommand, IEnumerable<IError>> func;
+
+        public LambdaCommandHandler(Func<TCommand, IEnumerable<IError>> func)
+        {
+            this.func = func;
+        }
+
+        public override Task<IEnumerable<IError>> Handle(TCommand command)
+        {
+            var result = func(command);
+            return Task.FromResult(result);
+        }
+    }
+}
