3c9a3b600579b0dadc830b45e2420b649dba7ef4
Dominik Weber <accounts@dominik-weber.name>
2016-08-28 20:29:07 +0200

    Add PropertyTrigger StateTrigger
    This makes it easier to trigger VisualStates.

---
+++ Willo.View/Infrastructure/StateTriggers/PropertyTrigger.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+
+namespace Willo.View.Infrastructure.StateTriggers
+{
+    public class PropertyTrigger : StateTriggerBase
+    {
+        public object Target
+        {
+            get { return (object)GetValue(TargetProperty); }
+            set { SetValue(TargetProperty, value); }
+        }
+        public static readonly DependencyProperty TargetProperty =
+            DependencyProperty.Register("Target", typeof(object), typeof(PropertyTrigger), new PropertyMetadata(null, Changed));
+
+        public object Value
+        {
+            get { return (object)GetValue(ValueProperty); }
+            set { SetValue(ValueProperty, value); }
+        }
+        public static readonly DependencyProperty ValueProperty =
+            DependencyProperty.Register("Value", typeof(object), typeof(PropertyTrigger), new PropertyMetadata(null, Changed));
+
+        private static void Changed(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var trigger = d as PropertyTrigger;
+            trigger.SetActive(trigger.Target == trigger.Value);
+        }
+    }
+}
