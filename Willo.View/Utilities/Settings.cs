666255d88babe10a0160df461ad45ff7c81b808f
Dominik Weber <accounts@dominik-weber.name>
2016-10-02 18:33:49 +0200

    Add Settings Utility that saves local User Settings

---
+++ Willo.View/Utilities/Settings.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tapi.WebConnection;
+using Windows.Storage;
+
+namespace Willo.View.Utilities
+{
+    public class Settings
+    {
+        private ApplicationDataContainer settings;
+
+        public Settings()
+        {
+            settings = ApplicationData.Current.LocalSettings;
+        }
+
+        public AuthorizationToken? AuthorizationToken
+        {
+            get { return (AuthorizationToken?)settings.Values["AuthorizationToken"]; }
+            set { settings.Values["AuthorizationToken"] = (string)value; }
+        }
+    }
+}
