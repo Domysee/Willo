0cc215c23d188c6ccc53bede2e9f92c8822457da
Dominik Weber <accounts@dominik-weber.name>
2016-10-02 10:54:24 +0200

    Move GetAll Documentation from Boards to IBoards
    Since the interface is always used, this improves intellisense.

--- Tapi/TrelloEntities/Boards.cs
+++ Tapi/TrelloEntities/Boards.cs
@@ -17,14 +17,6 @@ namespace Tapi.TrelloEntities.Board
             this.webClient = webClient;
         }
 
-        /// <summary>
-        /// gets all boards of the given user
-        /// </summary>
-        /// <param name="memberId">the user to get the boards from</param>
-        /// <param name="properties">the properties that should be returned by the api</param>
-        /// <returns></returns>
-        /// <exception cref="AuthorizationDeniedException">Occurs if the user revoked the authorization token</exception>
-        /// <exception cref="RequestFailedException">When the response status code does not indicate success</exception>
         public async Task<IEnumerable<Board>> GetAll(MemberId memberId, BoardProperties properties = null)
         {
             if (memberId == null) throw new ArgumentNullException("The given memberId is null. To use the authorized user, use MemberId.AuthorizedUser");
