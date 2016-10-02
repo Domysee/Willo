c911311897b35615063576e7d70b33aa8739f786
Dominik Weber <accounts@dominik-weber.name>
2016-10-02 11:02:29 +0200

    Add TrelloQueryHandlerBase
    TrelloQueryHandlerBase handles common exceptions thrown by Tapi and converts them to errors.
    This will reduce code duplication regarding exception handling.

---
+++ Willo.Logic/Infrastructure/TrelloQueryHandlerBase.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tapi.WebConnection;
+using Willo.Logic.Errors;
+
+namespace Willo.Logic.Infrastructure
+{
+    public abstract class TrelloQueryHandlerBase<TQuery, TReturn> : QueryHandlerBase<TQuery, TReturn>
+        where TQuery : IQuery<TReturn>
+    {
+        public override async Task<QueryResult<TReturn>> Handle(TQuery query)
+        {
+            try
+            {
+                return await handleImplementation(query);
+            }
+            catch (AuthorizationDeniedException)
+            {
+                var error = new AuthorizationDeniedError();
+                return QueryResult<TReturn>.CreateFailure(new[] { error });
+            }
+            catch (RequestFailedException)
+            {
+                var error = new RequestFailedError();
+                return QueryResult<TReturn>.CreateFailure(new[] { error });
+            }
+        }
+
+        protected abstract Task<QueryResult<TReturn>> handleImplementation(TQuery query);
+    }
+}
