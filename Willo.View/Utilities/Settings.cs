d92b4bbb63a5a0a359186974854fb3eb728c142c
Dominik Weber <accounts@dominik-weber.name>
2016-10-02 20:06:46 +0200

    Rework Settings to use a json File as Storage instead of LocalSettings
    LocalSettings didn't persist the settings for some reason, so I had to do it myself.

--- Willo.View/Utilities/Settings.cs
+++ Willo.View/Utilities/Settings.cs
@@ -1,7 +1,6 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using Tapi.WebConnection;
 using Windows.Storage;
@@ -10,17 +9,59 @@ namespace Willo.View.Utilities
 {
     public class Settings
     {
-        private ApplicationDataContainer settings;
+        private const string SettingsFilename = "Settings.json";
+        private Dictionary<string, object> settings = new Dictionary<string, object>();
 
         public Settings()
         {
-            settings = ApplicationData.Current.LocalSettings;
+            Load().Wait();
+        }
+
+        public async Task Load()
+        {
+            var folder = ApplicationData.Current.LocalFolder;
+            var settingsFile = await folder.TryGetItemAsync(SettingsFilename) as IStorageFile;
+
+            if (settingsFile != null)
+            {
+                var settingsJson = await FileIO.ReadTextAsync(settingsFile);
+                settings = JsonConvert.DeserializeObject<Dictionary<string, object>>(settingsJson);
+            }
+        }
+
+        public async Task Save()
+        {
+            var folder = ApplicationData.Current.LocalFolder;
+            var settingsFile = await folder.TryGetItemAsync(SettingsFilename) as IStorageFile;
+            if(settingsFile == null)
+                settingsFile = await folder.CreateFileAsync(SettingsFilename);
+
+            var settingsJson = JsonConvert.SerializeObject(settings);
+            await FileIO.WriteTextAsync(settingsFile, settingsJson);
+        }
+
+        private T? getStruct<T>(string key) where T : struct
+        {
+            if (settings.ContainsKey(key)) return (T)settings[key];
+            else return default(T);
+        }
+
+        private T getClass<T>(string key) where T : class
+        {
+            if (settings.ContainsKey(key)) return (T)settings[key];
+            else return default(T);
+        }
+
+        private void setValue(string key, object value)
+        {
+            settings[key] = value;
+            Save();
         }
 
         public AuthorizationToken? AuthorizationToken
         {
-            get { return (AuthorizationToken?)settings.Values["AuthorizationToken"]; }
-            set { settings.Values["AuthorizationToken"] = (string)value; }
+            get { return getClass<string>("AuthorizationToken"); }
+            set { setValue("AuthorizationToken", (string)value); }
         }
     }
 }
