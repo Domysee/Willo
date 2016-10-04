0bdc69a5b5c80acb52c65225df7a2f4092348bbb
Dominik Weber <accounts@dominik-weber.name>
2016-10-04 19:14:08 +0200

    Add Shell of BoardView

---
+++ Willo.View/Components/BoardView/BoardViewViewmodel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Willo.Logic.Infrastructure;
+using Willo.View.Infrastructure;
+
+namespace Willo.View.Components.BoardView
+{
+    public class BoardViewViewmodel : BindableBase
+    {
+        private IMessageBroker messageBroker;
+
+        public BoardViewViewmodel(IMessageBroker messageBroker)
+        {
+            this.messageBroker = messageBroker;
+        }
+
+        public async Task Initialize()
+        {
+
+        }
+    }
+}
