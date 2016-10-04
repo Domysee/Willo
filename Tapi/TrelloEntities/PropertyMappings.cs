550041beb6cfe884bda4500b5d079b8495a2e55a
Dominik Weber <accounts@dominik-weber.name>
2016-10-04 21:08:48 +0200

    Add PropertyMappings.CardFieldMappings

--- Tapi/TrelloEntities/PropertyMappings.cs
+++ Tapi/TrelloEntities/PropertyMappings.cs
@@ -35,5 +35,34 @@ namespace Tapi.TrelloEntities
             { nameof(Board.Board.Memberships), "memberships" },
             { nameof(Board.Board.LabelNames), "labelNames" }
         };
+
+        public static readonly Dictionary<string, string> CardFieldMappings = new Dictionary<string, string>
+        {
+            { nameof(Card.Card.Id), "id" },
+            { nameof(Card.Card.ShortId), "idShort" },
+            { nameof(Card.Card.Name), "name" },
+            { nameof(Card.Card.Description), "desc" },
+            { nameof(Card.Card.DescriptionData), "descData" },
+            { nameof(Card.Card.IsClosed), "closed" },
+            { nameof(Card.Card.IsUserSubscribed), "subscribed" },
+            { nameof(Card.Card.DueDate), "due" },
+            { nameof(Card.Card.Email), "email" },
+            { nameof(Card.Card.Badges), "badges" },
+            { nameof(Card.Card.CheckItemStates), "checkItemStates" },
+            { nameof(Card.Card.ContainingBoardId), "idBoard" },
+            { nameof(Card.Card.ContainingListId), "idList" },
+            { nameof(Card.Card.ContainedChecklistIds), "idChecklists" },
+            { nameof(Card.Card.MemberIds), "idMembers" },
+            { nameof(Card.Card.VotedMemberIds), "idMembersVoted" },
+            { nameof(Card.Card.AttachmentCoverId), "idAttachmentCover" },
+            { nameof(Card.Card.ManualCoverAttachment), "manualCoverAttachment" },
+            { nameof(Card.Card.Labels), "labels" },
+            { nameof(Card.Card.LabelIds), "idLabels" },
+            { nameof(Card.Card.LastActivity), "dateLastActivity" },
+            { nameof(Card.Card.Position), "pos" },
+            { nameof(Card.Card.Url), "url" },
+            { nameof(Card.Card.ShortUrl), "shortUrl" },
+            { nameof(Card.Card.ShortLink), "shortLink" }
+        };
     }
 }
