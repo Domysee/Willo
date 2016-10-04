1217f5001f540a291985c715d8314cf3cd2d8a71
Dominik Weber <accounts@dominik-weber.name>
2016-10-04 20:48:24 +0200

    Move PropertyMappings out of the Board Entity Folder
    This will be a mapping file for all entities, so it makes little sense to keep it in a folder
    of a specific entity.

--- Tapi/TrelloEntities/PropertyMappings.cs
+++ Tapi/TrelloEntities/PropertyMappings.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Tapi.TrelloEntities.Board
+namespace Tapi.TrelloEntities
 {
     /// <summary>
     /// contains mappings from properties of C# classes to the Trello api
