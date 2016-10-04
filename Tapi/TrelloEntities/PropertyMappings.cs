fa09baa1723a5913cea0462b341b98bd85366398
Dominik Weber <accounts@dominik-weber.name>
2016-10-04 21:07:43 +0200

    Rework PropertyMappings to use dynamic Property Strings

--- Tapi/TrelloEntities/PropertyMappings.cs
+++ Tapi/TrelloEntities/PropertyMappings.cs
@@ -13,27 +13,27 @@ namespace Tapi.TrelloEntities
     {
         public static readonly Dictionary<string, string> BoardFieldMappings = new Dictionary<string, string>
         {
-            {"Id", "id"},
-            {"Name", "name"},
-            {"Description", "desc"},
-            {"DescriptionData", "descData"},
-            {"IsClosed", "closed"},
-            {"IsUserInvited", "invited"},
-            {"IsUserSubscribed", "subscribed"},
-            {"IsStarredByUser", "starred"},
-            {"LastActivity", "dateLastActivity" },
-            {"LastView", "dateLastView"},
-            {"Url","url" },
-            {"ShortUrl", "shortUrl" },
-            {"ShortLink", "shortLink"},
-            {"Pinned", "pinned"},
-            {"OrganizationId", "idOrganization"},
-            {"Invitations", "invitations" },
-            {"PowerUps", "powerUps"},
-            {"IdTags", "idTags" },
-            {"Preferences","prefs" },
-            {"Memberships","memberships" },
-            {"LabelNames" ,"labelNames"}
+            { nameof(Board.Board.Id), "id" },
+            { nameof(Board.Board.Name), "name" },
+            { nameof(Board.Board.Description), "desc" },
+            { nameof(Board.Board.DescriptionData), "descData" },
+            { nameof(Board.Board.IsClosed), "closed" },
+            { nameof(Board.Board.IsUserInvited), "invited" },
+            { nameof(Board.Board.IsUserSubscribed), "subscribed" },
+            { nameof(Board.Board.IsStarredByUser), "starred" },
+            { nameof(Board.Board.LastActivity), "dateLastActivity" },
+            { nameof(Board.Board.LastView), "dateLastView" },
+            { nameof(Board.Board.Url), "url" },
+            { nameof(Board.Board.ShortUrl), "shortUrl" },
+            { nameof(Board.Board.ShortLink), "shortLink" },
+            { nameof(Board.Board.Pinned), "pinned" },
+            { nameof(Board.Board.OrganizationId), "idOrganization" },
+            { nameof(Board.Board.Invitations), "invitations" },
+            { nameof(Board.Board.PowerUps), "powerUps" },
+            { nameof(Board.Board.IdTags), "idTags" },
+            { nameof(Board.Board.Preferences), "prefs" },
+            { nameof(Board.Board.Memberships), "memberships" },
+            { nameof(Board.Board.LabelNames), "labelNames" }
         };
     }
 }
