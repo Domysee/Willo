6e7f6c5d9a8c92bb4bb7e2db63df3e1765ce28aa
Dominik Weber <accounts@dominik-weber.name>
2016-10-05 20:02:40 +0200

    Add Label Entity Class

---
+++ Tapi/TrelloEntities/Label/Label.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tapi.TrelloEntities.Label
+{
+    public class Label
+    {
+        public string Id { get; private set; }
+        public string Name { get; private set; }
+        public string ContainingBoardId { get; private set; }
+        public string Color { get; private set; }
+        public int UseCount { get; private set; }
+    }
+}
