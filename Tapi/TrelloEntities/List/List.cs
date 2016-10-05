1d8554e3081277a85dc0bc469cbf1b61d637cb1f
Dominik Weber <accounts@dominik-weber.name>
2016-10-05 20:09:49 +0200

    Add List Entity Class

---
+++ Tapi/TrelloEntities/List/List.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tapi.TrelloEntities.List
+{
+    public class List
+    {
+        public string Id { get; private set; }
+        public string Name { get; private set; }
+        public bool IsClosed { get; private set; }
+        public bool IsUserSubscribed { get; private set; }
+        public string ContainingBoardId { get; private set; }
+        public int Position { get; private set; }
+    }
+}
