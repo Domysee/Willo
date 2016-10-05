3bce2f95f9df239c74f1e52077468fda07293b56
Dominik Weber <accounts@dominik-weber.name>
2016-10-05 22:05:55 +0200

    Add LabelFields Class

---
+++ Tapi/TrelloEntities/Label/LabelFields.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tapi.TrelloEntities.Label
+{
+    public class LabelFields
+    {
+        public bool Id { get; private set; }
+        public bool Name { get; private set; }
+        public bool ContainingBoardId { get; private set; }
+        public bool Color { get; private set; }
+        public bool UseCount { get; private set; }
+
+        public static readonly LabelFields All = new LabelFields
+        {
+            Id = true,
+            Name = true,
+            ContainingBoardId = true,
+            Color = true,
+            UseCount = true
+        };
+    }
+}
