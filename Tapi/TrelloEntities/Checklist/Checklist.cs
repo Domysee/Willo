bd71cf1286565084b0c311ff7eb08f7830935383
Dominik Weber <accounts@dominik-weber.name>
2016-10-05 19:58:37 +0200

    Add Checklist Entity Class

---
+++ Tapi/TrelloEntities/Checklist/Checklist.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tapi.TrelloEntities.Checklist
+{
+    public class Checklist
+    {
+        public string Id { get; private set; }
+        public string Name { get; private set; }
+        public string ContainingBoardId { get; private set; }
+        public string ContainingCardId { get; private set; }
+        public int Position { get; private set; }
+    }
+}
