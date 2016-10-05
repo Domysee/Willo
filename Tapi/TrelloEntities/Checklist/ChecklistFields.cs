44aa9e17ad62f114e37f47e91fe7a8d4f25b5a53
Dominik Weber <accounts@dominik-weber.name>
2016-10-05 21:57:34 +0200

    Add ChecklistFields Class

---
+++ Tapi/TrelloEntities/Checklist/ChecklistFields.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tapi.TrelloEntities.Checklist
+{
+    public class ChecklistFields
+    {
+        public bool Id { get; set; }
+        public bool Name { get; set; }
+        public bool ContainingBoardId { get; set; }
+        public bool ContainingCardId { get; set; }
+        public bool Position { get; set; }
+
+        public static readonly ChecklistFields All = new ChecklistFields
+        {
+            Id = true,
+            Name = true,
+            ContainingBoardId = true,
+            ContainingCardId = true,
+            Position = true
+        };
+    }
+}
