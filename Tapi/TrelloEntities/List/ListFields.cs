9f8806f4d1db87c55d90acdd4342b45026668060
Dominik Weber <accounts@dominik-weber.name>
2016-10-05 22:06:04 +0200

    Add ListFields Class

---
+++ Tapi/TrelloEntities/List/ListFields.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tapi.TrelloEntities.List
+{
+    public class ListFields
+    {
+        public bool Id { get; private set; }
+        public bool Name { get; private set; }
+        public bool IsClosed { get; private set; }
+        public bool IsUserSubscribed { get; private set; }
+        public bool ContainingBoardId { get; private set; }
+        public bool Position { get; private set; }
+
+        public static readonly ListFields All = new ListFields
+        {
+            Id = true,
+            Name = true,
+            IsClosed = true,
+            IsUserSubscribed = true,
+            ContainingBoardId = true,
+            Position = true
+        };
+    }
+}
