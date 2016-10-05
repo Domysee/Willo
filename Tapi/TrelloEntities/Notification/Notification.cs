35708475a2fa06569859fbdc5df070d3c9de2b4b
Dominik Weber <accounts@dominik-weber.name>
2016-10-05 21:44:27 +0200

    Add Notification Entity Class

---
+++ Tapi/TrelloEntities/Notification/Notification.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tapi.TrelloEntities.Notification
+{
+    public class Notification
+    {
+        public string Id { get; private set; }
+        public DateTime Date { get; private set; }
+        public string CreatorId { get; private set; }
+        public string Type { get; private set; }
+        public bool IsUnread { get; private set; }
+        public object Data { get; private set; }
+    }
+}
