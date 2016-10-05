ef26a9e721a9d916ecf86cbf6000c522325ea1f9
Dominik Weber <accounts@dominik-weber.name>
2016-10-05 20:20:21 +0200

    Add Action Entity Class

---
+++ Tapi/TrelloEntities/Action/Action.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tapi.TrelloEntities.Action
+{
+    public class Action
+    {
+        public string Id { get; private set; }
+        public MemberId CreatorId { get; private set; }
+        public string Type { get; private set; }
+        public object Data { get; private set; }
+    }
+}
