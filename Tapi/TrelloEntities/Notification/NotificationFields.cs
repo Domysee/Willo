0287759e8e996ec4d909f463650816d26e3ba2fc
Dominik Weber <accounts@dominik-weber.name>
2016-10-05 22:06:25 +0200

    Add NotificationFields Class

---
+++ Tapi/TrelloEntities/Notification/NotificationFields.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tapi.TrelloEntities.Notification
+{
+    public class NotificationFields
+    {
+        public bool Id { get; private set; }
+        public bool Date { get; private set; }
+        public bool CreatorId { get; private set; }
+        public bool Type { get; private set; }
+        public bool IsUnread { get; private set; }
+        public bool Data { get; private set; }
+
+        public static readonly NotificationFields All = new NotificationFields
+        {
+            Id = true,
+            Date = true,
+            CreatorId = true,
+            Type = true,
+            IsUnread = true,
+            Data = true
+        };
+    }
+}
