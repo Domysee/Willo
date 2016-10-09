b549212ec6ae3351892f71b27864329914080b5b
Dominik Weber <accounts@dominik-weber.name>
2016-10-09 15:19:37 +0200

    Improve CommentPermission Naming and Documentation

--- Tapi/TrelloEntities/Board/CommentPermission.cs
+++ Tapi/TrelloEntities/Board/CommentPermission.cs
@@ -8,10 +8,10 @@ namespace Tapi.TrelloEntities.Board
 {
     public enum CommentPermission
     {
-        Disabled,
-        Members,
-        Observers,
-        Team,
-        Public
+        Disabled,   //No one is allowed to comment
+        Members,    //Allow admins and normal members to comment
+        MembersAndObservers,    //Allow admins, normal members, and observers to comment
+        Team,   //Allow admins, normal members, observers, and team members to comment
+        Public  //Allow any Trello member to comment
     }
 }
