8b57808169f53df087b1239c914205e6aaccd420
Dominik Weber <accounts@dominik-weber.name>
2016-11-16 23:00:37 +0100

    Implement Action Conversion from Json

--- Tapi/TrelloEntities/Action/Action.cs
+++ Tapi/TrelloEntities/Action/Action.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using Tapi.TrelloEntities.Member;
@@ -13,5 +15,24 @@ namespace Tapi.TrelloEntities.Action
         public MemberId CreatorId { get; private set; }
         public DateTime Date { get; private set; }
         public ActionOwner Executor { get; private set; }
+
+        public static Action FromJson(JObject jobject)
+        {
+            Action action = null;
+            var actionTypeName = jobject.Value<string>("type");
+            var actionType = typeof(Action).GetTypeInfo().Assembly.DefinedTypes
+                .Where(t => t.Namespace == nameof(Tapi.TrelloEntities.Action.ActionTypes))
+                .Where(t => String.Equals(t.Name, actionTypeName, StringComparison.OrdinalIgnoreCase));
+            foreach (var property in action.GetType().GetProperties())
+            {
+                var apiPropertyName = PropertyMappings.CardFieldMappings[property.Name];
+                var jToken = jobject[apiPropertyName];
+                if (jToken == null) //the subclass properties are defined in the data property
+                    jToken = jobject["data"][apiPropertyName];
+                if (jToken != null)
+                    property.SetValue(action, jToken.ToObject(property.PropertyType));
+            }
+            return action;
+        }
     }
 }
